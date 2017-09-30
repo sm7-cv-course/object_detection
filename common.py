f3fe905ec2ac869dfbfe40d87729021da1d784f2
Evgeny <shtanov_evgenii@mail.ru>
2017-09-30 11:49:21 +0300

    Initial commit. HOG+SVM classifier, Pyramidal sliding window detector. HOG parameters suits for wheel detection on side-scan sonar images.
    Signed-off-by: Evgeny <shtanov_evgenii@mail.ru>

---
+++ common.py
@@ -0,0 +1,42 @@
+import cv2
+import numpy as np
+
+def get_hog() :
+    winSize = (20,20)
+    blockSize = (8,8)
+    blockStride = (4,4)
+    cellSize = (8,8)
+    nbins = 9
+    derivAperture = 1
+    winSigma = -1.
+    histogramNormType = 0
+    L2HysThreshold = 0.2
+    gammaCorrection = 1
+    nlevels = 64
+    signedGradient = True
+
+    hog = cv2.HOGDescriptor(winSize,blockSize,blockStride,cellSize,nbins,derivAperture,winSigma,histogramNormType,L2HysThreshold,gammaCorrection,nlevels,signedGradient)
+
+    return hog
+    affine_flags = cv2.WARP_INVERSE_MAP|cv2.INTER_LINEAR
+
+class StatModel(object):
+    def load(self, fn):
+        # self.model.load(fn)  # Known bug: https://github.com/opencv/opencv/issues/4969
+        self.model = cv2.ml.SVM_load(fn)
+    def save(self, fn):
+        self.model.save(fn)
+
+class SVM(StatModel):
+    def __init__(self, C = 12.5, gamma = 0.50625):
+        self.model = cv2.ml.SVM_create()
+        self.model.setGamma(gamma)
+        self.model.setC(C)
+        self.model.setKernel(cv2.ml.SVM_RBF)
+        self.model.setType(cv2.ml.SVM_C_SVC)
+
+    def train(self, samples, responses):
+        self.model.train(samples, cv2.ml.ROW_SAMPLE, responses)
+
+    def predict(self, samples):
+        return self.model.predict(samples)[1].ravel()
--- license.md
+++ license.md
@@ -1,3 +1,4 @@
 This is computer vision course material for educational purpose only.  
 Some codes are taken from pyimagesearch.com, learnopencv.com.  
 Images, titled with Gonzalez are taken from disk supplied with book "Digital Image Processing Using MATLAB".  
+Side-scan sonar images are given by Screen Co, ООО "Экран" hydrasonars.ru, screen-co.ru
